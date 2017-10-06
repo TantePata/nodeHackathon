ef9741dc4ceca92b336f6a3b378d27c1891aaf49
= <herve@coignard.xyz>
2017-10-06 11:59:30 +0200

    delete answ

--- actions/answers/crud.js
+++ actions/answers/crud.js
@@ -130,7 +130,7 @@ module.exports = (api) => {
                 id: req.params.id
             }
         }).then(function() {
-            return res.status(200).send("{}");
+            return res.status(200).send("{toto:toto}");
         }).catch(function(error) {
             return res.status(500).send(error)
         });
